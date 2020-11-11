c7eb5436c29b3ec9010e64f8c1c53c730e676596
Justin Saparco-Ueno <jsaparco.ueno@gmail.com>
2020-11-11 04:19:03 -0700

    Add clear() in between console writes; Take a minutes command line argument

--- Program.cs
+++ Program.cs
@@ -8,21 +8,33 @@ namespace countdn
         static void Main(string[] args)
         {
             int minutes = 15;
+
+            if (args.Length == 1)
+            {
+                int numMinutes = 15;
+                var isValidMins = int.TryParse(args[0], out numMinutes);
+                if (isValidMins) minutes = numMinutes;
+            }
             string timerDoneText = "lift\noff!";
             BeginCountDown(minutes, timerDoneText);
         }
 
         static private void BeginCountDown(int minutes, string timerDoneText)
         {
-            Console.WriteLine("Beginning countdown");
+            var countdnPath = @"D:\streamtools\countdn.txt";
+            Console.Clear();
+            Console.WriteLine($"Beginning countdown from {ToTimerFormat(minutes * 60)}");
+            Thread.Sleep(3000);
             for (int seconds = minutes * 60; seconds > 0; seconds--)
             {
-                System.IO.File.WriteAllText(@"D:\projects\countdn\countdn.txt", ToTimerFormat(seconds));
+                Console.Clear();
+                System.IO.File.WriteAllText(countdnPath, ToTimerFormat(seconds));
                 Console.WriteLine(ToTimerFormat(seconds));
                 Thread.Sleep(1000);
             }
+            Console.Clear();
             Console.WriteLine(timerDoneText);
-            System.IO.File.WriteAllText(@"D:\projects\countdn\countdn.txt",timerDoneText);
+            System.IO.File.WriteAllText(countdnPath,timerDoneText);
         }
 
         static private string ToTimerFormat(int seconds)
